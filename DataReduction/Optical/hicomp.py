ad5b321a6eacf9af6b2571e98ceed333ffb4504f
George C. Privon <george@privon.com>
2012-05-14 03:12:46 -0700

    hicomp.py: slight generalization
    - should now work for arbitrary numbers of FITS extensions (assuming
      both the files have the same number of extensions)
    - improved usefulness of output when plots are being saved
    - deleted some unnecessary lines

--- DataReduction/Optical/hicomp.py
+++ DataReduction/Optical/hicomp.py
@@ -12,14 +12,16 @@ def hicomp(files,compfile=False,hrange=(0,65000),corrfac=1):
 
   # get the flie list
   files=glob.glob(files)
+  # sort it
+  files.sort()
   if compfile:
     # load the compfile, compare *everything* to this
     frame=pyfits.open(compfile)
     comp=True
   else:
     comp=False
-  # continue comparing stufffff
-  j=0
+  # continue comparing stuff
+  j=0	# keep track of how many comparisons we've made
   for file in files:
     plt.figure(j)
     plt.xlabel('Counts')
@@ -29,8 +31,9 @@ def hicomp(files,compfile=False,hrange=(0,65000),corrfac=1):
       frame=pyfits.open(file.split('.fits')[0]+'-bsub_flat.fits')
       plt.title(file+'-'+file.split('.fits')[0]+'-bsub_flat.fits')
     else:
-      plt.title(file+'-'+compfile)
-    for i in range(1,17):
+      plt.title(file+' and '+compfile)
+    for i in range(1,len(frame)):	# frames must have the same number of
+					# extensions for this to work!
       # uncorrected data
       (a,b,c)=plt.hist(numpy.ravel(frame2[i].data),100,range=hrange,alpha=0.3,color='red')
       # corrected data
@@ -38,12 +41,15 @@ def hicomp(files,compfile=False,hrange=(0,65000),corrfac=1):
     frame2.close()
     if not(comp):
       frame.close()
-    print "Saving figure "+str(j)
+    print "Saving figure #"+str(j)+", "+file.split('.fits')[0]+'.png'
     plt.savefig(file.split('.fits')[0]+'.png',format='png')
     j=j+1
-    #if i>2:
-    #  break
 
   if comp:
     frame.close()
-  #plt.show()
+
+  # delete unused variables
+  del a,b,c
+  del frame,frame2
+
+  return 0
