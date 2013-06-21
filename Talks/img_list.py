ce9fa33b2580604898c832ff96ce312711982d4c
George Privon <gcp8y@virginia.edu>
2013-06-21 15:16:33 -0700

    maintain images in directories for talks
    search a tex file for images in 'includegraphics' enviros and compare
    with files in the current directory to generate a list of files that
    can be removed.

---
+++ Talks/img_list.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+#
+# Make a list of unique images files in a tex file, get list of images in 
+# that directory, and print out which aren't being used in that tex file.
+
+import re,sys,os
+
+suffix=['jpg','png','gif','pdf']
+
+try: 
+  texfile=open(sys.argv[1])
+except:
+  sys.stderr.write("Error, couldn't open "+sys.argv[1]+" for reading.\n")
+
+imglist=[]
+rmlist=[]
+
+for line in texfile:
+  if re.search('includegraphics',line):
+    sline=line.split('\\includegraphics')
+    print sline
+    n=len(sline)-1
+    for i in range(1,n+1):
+      img=sline[i].split(']{')[1].split('}')[0]
+      if not(img in imglist): imglist.append(img)
+
+for filename in os.listdir('.'):
+  nomatch=True
+  i=0
+  while nomatch:
+    if re.search(suffix[i],filename):
+      nomatch=False
+      if not(filename.split('\/')[-1] in imglist):
+        rmlist.append(filename.split('\/')[-1])
+    i+=1
+    if i>=len(suffix): break
+
+imglist.sort()
+sys.stderr.write(str(len(imglist))+" images found.\n")
+sys.stdout.write("Found images: ")
+for img in imglist:
+  sys.stdout.write(img+", ")
+sys.stdout.write("\n")
+sys.stdout.write("The unused images can be removed with this command:\n")
+sys.stdout.write("rm ")
+for img in rmlist:
+  sys.stdout.write(img+' ')
