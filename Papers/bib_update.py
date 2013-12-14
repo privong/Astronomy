a142e587a9d64b07288fe4e54587cc737a2ceeda
George C. Privon <george@privon.com>
2013-12-14 10:55:13 -0500

    initial code to find records

--- Papers/bib_update.py
+++ Papers/bib_update.py
@@ -3,3 +3,13 @@
 # Load a bibtext file, find the arXiv entries within the past X years and 
 # query ADS to see if the paper has been published. If so, update the bibtex
 # entry.
+
+import pycurl,bibtexparser
+
+# get ADSABS dev key
+of=open('ADS_Key','r')
+key='dev_key='+of.readline().rstrip()
+of.close()
+
+urlbase='http://adslabs.org/adsabs/api/search/?fmt=json&'+key
+recordbase='http://adslabs.org/adsabs/api/record/'
