924e8fa994e0d6a63cc5e62e196849c337b76227
George C. Privon <george@privon.com>
2013-12-15 06:57:31 -0500

    DictStruct: stderr -> stdout

--- General/mysci.py
+++ General/mysci.py
@@ -235,8 +235,8 @@ def DictStruct(d,depth=0):
   if type(d)==dict:
     for key in d.keys():
       for i in range(depth):
-        sys.stderr.write("  ")
-      sys.stderr.write(str(key)+"\n")
+        sys.stdout.write("  ")
+      sys.stdout.write(str(key)+"\n")
       DictStruct(d[key],depth=depth+1)
 
 # End Convenience functions
