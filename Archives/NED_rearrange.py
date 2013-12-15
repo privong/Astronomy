f6e0bc8d2ad7753b47bb9ec638abb997d40baa04
George C. Privon <george@privon.com>
2013-12-15 08:46:50 -0700

    NED_rearrange: sample processing of NED batch results

---
+++ Archives/NED_rearrange.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+#
+# Process NED data
+
+import sys,re
+
+fin=open(sys.argv[1],'r')
+out=open('NED-reformat.dat','w')
+go=0
+
+out.write('#Source\t12\t25\t60\t100')
+
+for line in fin:
+  if go:
+    # do stuff
+    if re.search('PHOTO',line):
+      # extract the source name
+      out.write('\n')
+      out.write(''.join(str(line.split()[1:])))
+      out.write('\t')
+    if re.search('IRAS',line):
+      if not(re.search('targeted',line)) and not(re.search('Qualifiers',line)) and not(re.search('refcode',line)):
+        out.write(line.split()[4])
+        out.write('\t')
+  elif re.search('SEARCH RESULTS',line):
+    go=1
+
+fin.close()
+out.close()
