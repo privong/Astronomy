390da5227deb45e0f8b0f5d144e0bc2fdd151b4d
George C. Privon <george@privon.com>
2014-07-28 17:41:25 -0400

    force python2

--- Papers/bib_update.py
+++ Papers/bib_update.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 #
 # Load a bibtext file, find the arXiv entries within the past X years and
 # query ADS to see if the paper has been published. If so, update the bibtex
