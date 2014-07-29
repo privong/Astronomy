c0a302c8c4e2df1db29af3c49bf809707174c611
George C. Privon <george@privon.com>
2014-07-29 16:24:46 -0400

    module and function fixes. slight code tweaks

--- General/vcosmic.py
+++ General/vcosmic.py
@@ -1,6 +1,7 @@
 # Compute luminosity distance for a 3-attractor model (Mould+ 2000)
 
 import numpy as np
+import astropy.units as u
 import mysci
 
 def vcosmic(vh,b1950ra,b1950dec):
@@ -26,38 +27,37 @@ def vcosmic(vh,b1950ra,b1950dec):
     glat=np.arcsin(singlat)
     cosglat=np.sqrt(1.-singlat*singlat)
     # galactic longitude
-    if cosglat==0:
+    if cosglat==0.:
         glon=0.*u.rad
     cosglon=cosdec * cosrara0 / cosglat
     singlon = (cosdec * sinrara0 * coscdec0 + sindec * sincdec0)/cosglat
-    if cosglon!=0:
-        glon = atan (singlon / cosglon)
+    if cosglon!=0.:
+        glon = np.arctan (singlon / cosglon)
     else:
-        if singlon > 0: glon = np.pi/2.*u.rad
-        if singlon < 0: glon = 1.5*np.pi*u.rad
+        if singlon > 0.: glon = np.pi/2.*u.rad
+        if singlon < 0.: glon = 1.5*np.pi*u.rad
     # resolve 180 degree ambiguity in glon
-    if cosglon <0: glon = glon + np.pi*u.rad
-    glon = glon + glon0
+    if cosglon < 0.: glon = glon + np.pi*u.rad
+    glon = glon + mysci.galcenter['galLon']
     twopi = 2. * np.pi*u.rad
     if glon < 0: glon = glon + twopi
-    if glon > twopi: glon = glon - twopi
-
+    if glon >= twopi: glon = glon - twopi
 
     # correct from heliocentric to local group velocity
-    vlg = vh - 79. *np.cos(glon)*np.cos(glat) \
-            + 296.*np.sin(glon)*np.cos(glat) \
-            - 36. *np.sin(glat)
+    vlg = vh - 79.*u.km/u.s*np.cos(glon)*np.cos(glat) \
+            + 296.*u.km/u.s*np.sin(glon)*np.cos(glat) \
+            - 36.*u.km/u.s*np.sin(glat)
     vcosmic=vlg
 
     # correct for infall velocities
     # reformat attractor constants
-    racl=[VirgoCluster['RA'],GreatAttractor['RA'],ShapleySupercluster['RA']]
-    deccl=[VirgoCluster['Dec'],GreatAttractor['Dec'],ShapleySupercluster['Dec']]
-    vlgcl=[VirgoCluster['vlgcl'],GreatAttractor['vlgcl'],ShapleySupercluster['vlgcl']]
-    vfid=[VirgoCluster['vfid'],GreatAttractor['vfid'],ShapleySupercluster['vfid']]
-    radius=[VirgoCluster['radius'],GreatAttractor['radius'],ShapleySupercluster['radius']]
-    vmin=[VirgoCluster['vmin'],GreatAttractor['vmin'],ShapleySupercluster['vmin']]
-    vmax=[VirgoCluster['vmax'],GreatAttractor['vmax'],ShapleySupercluster['vmax']]
+    racl=[mysci.VirgoCluster['RA'],mysci.GreatAttractor['RA'],mysci.ShapleySupercluster['RA']]
+    deccl=[mysci.VirgoCluster['Dec'],mysci.GreatAttractor['Dec'],mysci.ShapleySupercluster['Dec']]
+    vlgcl=[mysci.VirgoCluster['vlgcl'],mysci.GreatAttractor['vlgcl'],mysci.ShapleySupercluster['vlgcl']]
+    vfid=[mysci.VirgoCluster['vfid'],mysci.GreatAttractor['vfid'],mysci.ShapleySupercluster['vfid']]
+    radius=[mysci.VirgoCluster['radius'],mysci.GreatAttractor['radius'],mysci.ShapleySupercluster['radius']]
+    vmin=[mysci.VirgoCluster['vmin'],mysci.GreatAttractor['vmin'],mysci.ShapleySupercluster['vmin']]
+    vmax=[mysci.VirgoCluster['vmax'],mysci.GreatAttractor['vmax'],mysci.ShapleySupercluster['vmax']]
     for i in range (3):
         x=np.sin(np.pi*u.rad/2.-b1950dec)*np.cos(b1950ra)
         y=np.sin(np.pi*u.rad/2.-b1950dec)*np.sin(b1950ra)
@@ -67,20 +67,21 @@ def vcosmic(vh,b1950ra,b1950dec):
         zcl = np.cos (np.pi*u.rad / 2. - deccl[i])
         dist = (x - xcl)**2 + (y - ycl)**2 + (z - zcl)**2
         dist = np.sqrt(dist)
-        theta = 2. * np.asin(dist / 2.)
+        theta = 2. * np.arcsin(dist / 2.)
         costheta = np.cos(theta)
 
         # is the galaxy in the attractor core cone?
         if theta < radius[i] and vh > vmin[i] and vh < vmax[i]:
             vcosmic=vlgcl[i]
             return vcosmic
-        roa=vlg**2 + vlgcl[i]**2 - 2. * vlg * vlgcl[i] * costheta
-        roa=roa**0.5
-        vinlg = vfid[i] * costheta
-        vingal = (vlg - vlgcl[i] * costheta) / roa
-        vingal = vingal * vfid[i] * vlgcl[i] / roa
-        vin = vinlg + vingal
-        vin = -vin
-        vcosmic = vcosmic - vin
+        else:
+            roa=vlg**2 + vlgcl[i]**2 - 2. * vlg * vlgcl[i] * costheta
+            roa=np.sqrt(roa)
+            vinlg = vfid[i] * costheta
+            vingal = (vlg - vlgcl[i] * costheta) / roa
+            vingal = vingal * vfid[i] * vlgcl[i] / roa
+            vin = vinlg + vingal
+            vin = -vin
+            vcosmic = vcosmic - vin
 
         return vcosmic
