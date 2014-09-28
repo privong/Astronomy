1775e60bd5a05450988861229d28cd932230b7d9
George C. Privon <george@privon.com>
2014-09-28 12:04:58 -0300

    better comments, pep8

--- DataAnalysis/abs_err.py
+++ DataAnalysis/abs_err.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python
 #
-# abs_err cont cont_err abs abs_err
+# abs_err cont cont_err abso abs_err
 #
-# Takes inputs of fluxes in the same units and computes the error in the measured opacity
+# Compute optical depth and its error, using an input absorption measurement
+# and continuum measurement. All (integrated) fluxes must be in the same units.
 
 import math
 import sys
 
-cont=float(sys.argv[1])
-cerr=float(sys.argv[2])
-abs=float(sys.argv[3])
-aerr=float(sys.argv[4])
 
-tau=-1*math.log(1.-abs/cont)
-#terrsq=(1/(1-abs/cont))**2*(aerr**2/cont+(cerr**2*abs)/cont**2)
-terrsq=(1/(1-abs/cont))**2*((aerr/cont)**2+abs**2*cerr**2/cont**4)
+cont = float(sys.argv[1])
+cerr = float(sys.argv[2])
+abso = float(sys.argv[3])
+aerr = float(sys.argv[4])
+
+tau = -1*math.log(1.-abso/cont)
+# terrsq = (1/(1-abso/cont))**2*(aerr**2/cont+(cerr**2*abso)/cont**2)
+terrsq = (1/(1-abso/cont))**2*((aerr/cont)**2+abso**2*cerr**2/cont**4)
 
 print "tau: {0:.4f}".format(tau)
 print "delta tau: {0:.4f}".format(math.sqrt(terrsq))
-
-
