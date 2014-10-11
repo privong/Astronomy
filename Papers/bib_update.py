48f78063bd17d3fa5ba600b74dd9d5a515ea41a1
George C. Privon <george@privon.com>
2014-10-11 00:27:41 -0300

    pep8

--- Papers/bib_update.py
+++ Papers/bib_update.py
@@ -15,7 +15,7 @@ import ads  # https://github.com/andycasey/ads
 import argparse
 
 
-def checkRef(entry,confirm=False):
+def checkRef(entry, confirm=False):
     """
     Check ADS for an updated reference
 
@@ -30,20 +30,21 @@ def checkRef(entry,confirm=False):
         - pub(lication) (does not currently do abbreviations)
     """
     if 'eprint' in entry.keys():
-        res = ads.query("arXiv:"+entry['eprint'])
+        res = ads.query("arXiv:" + entry['eprint'])
         for i in res:
             try:    # only want things that are published
                 print i.pub
-            except :
+            except:
                 continue
             if confirm:
-                print entry['author'].split(',')[0],entry['title'],entry['year']
-                print i.author[0],i.title[0],i.year
+                print entry['author'].split(',')[0], entry['title'], \
+                      entry['year']
+                print i.author[0], i.title[0], i.year
                 sel = raw_input('Is this a match (y/n)? ')
             else:
                 sel = 'y'
             if sel == 'y':  # replace relevant bibtex entries
-                entry['author'] = '{'+entry['author']+'}'
+                entry['author'] = '{' + entry['author'] + '}'
                 entry['title'] = i.title[0]
                 entry['year'] = i.year
                 entry['journal'] = i.pub
@@ -51,85 +52,98 @@ def checkRef(entry,confirm=False):
                     entry['doi'] = i.doi[0]
                 except:
                     pass
-                try: 
+                try:
                     entry['abstract'] = i.abstract
                 except:
                     pass
                 entry['link'] = i.url[0]
                 entry['year'] = i.year
-                try: 
+                try:
                     entry['volume'] = i.volume
                 except:
                     pass
-                try: 
+                try:
                     entry['pages'] = i.page[0]
                 except:
                     pass
-                entry['adsurl'] = 'http://adsabs.harvard.edu/abs/'+i.bibcode
-                if not(re.search(i.year,entry['id'])):
-                    sys.stderr.write("Warning: Updating year of: "+entry['id']+" to reflect publication year ("+i.year+").\n")
-                    entry['id'] = entry['id'].split('2')[0]+i.year
+                entry['adsurl'] = 'http://adsabs.harvard.edu/abs/' + i.bibcode
+                if not(re.search(i.year, entry['id'])):
+                    sys.stderr.write("Warning: Updating year of: " +
+                                     entry['id'] +
+                                     " to reflect publication year (" +
+                                     i.year + ").\n")
+                    entry['id'] = entry['id'].split('2')[0] + i.year
                 return entry
         return False
 
 
-parser=argparse.ArgumentParser(description="Update arXiv entries in a bibtex file with subsequently published papers.")
-parser.add_argument('bibfile',action='store',type=str,default=False,help='BibTeX file')
-parser.add_argument('--confirm',action='store_true',default=False,help='If passed, confirm each entry.')
-args=parser.parse_args()
+parser = argparse.ArgumentParser(description="Update arXiv entries in a bibtex
+                                              file with subsequently published
+                                              papers.")
+parser.add_argument('bibfile', action='store', type=str, default=False,
+                    help='BibTeX file')
+parser.add_argument('--confirm', action='store_true', default=False,
+                    help='If passed, confirm each entry.')
+args = parser.parse_args()
 
 if os.path.isfile(args.bibfile):
-    bib = codecs.open(args.bibfile,'r','utf-8')
+    bib = codecs.open(args.bibfile, 'r', 'utf-8')
     bp = BibTexParser(bib.read())
     bib.close()
 else:
-    sys.stderr.write("Error, could not open: "+args.bibfile+".\n")
+    sys.stderr.write("Error, could not open: " + args.bibfile + ".\n")
     sys.exit(1)
 
 # back up library before we start
-shutil.copy2(args.bibfile,args.bibfile+'ads_updater.bak')
+shutil.copy2(args.bibfile, args.bibfile + 'ads_updater.bak')
 
 upcount = 0
 match = False
 j = 0
 for j in range(len(bp.records)):
-    thisref=bp.records[j]
-    if thisref['type']=='article':  # not interested in anything else
+    thisref = bp.records[j]
+    if thisref['type'] == 'article':  # not interested in anything else
         if 'journal' in thisref.keys():
-            if re.search('arxiv',thisref['journal']) or re.search('arXiv',thisref['journal']):
+            if re.search('arxiv', thisref['journal']) or \
+               re.search('arXiv', thisref['journal']):
                 match = True
         elif 'Journal' in thisref.keys():
-            if re.search('arxiv',thisref['Journal']) or re.search('arXiv',thisref['Journal']):
+            if re.search('arxiv', thisref['Journal']) or \
+               re.search('arXiv', thisref['Journal']):
                 match = True
         else:
             if 'arxivid' in thisref.keys():
                 match = True
             else:
-                sys.stdout.write(thisref['id']+' does not have a journal entry or arXiv ID.\n')
+                sys.stdout.write(thisref['id'] +
+                                 ' does not have a journal entry or arXiv
+                                  ID.\n')
 
         if match:
-            match = False # reset
-            sys.stdout.write('Searching for update to '+thisref['id']+'...')
-            res = checkRef(thisref,args.confirm)
+            match = False   # reset
+            sys.stdout.write('Searching for update to ' + thisref['id'] +
+                             '...')
+            res = checkRef(thisref, args.confirm)
             if res:
                 upcount += 1
                 bp.records[j] = res
-                sys.stdout.write(thisref['id']+" updated. Please verify changes.\n")
+                sys.stdout.write(thisref['id'] +
+                                 " updated. Please verify changes.\n")
                 newbib = to_bibtex(bp)
                 # replace bibtex file as we go, every 10 fixes :)
                 if upcount % 20 == 0:
-                    outf = codecs.open(args.bibfile,'w','utf-8')
+                    outf = codecs.open(args.bibfile, 'w', 'utf-8')
                     outf.write(newbib)
                     outf.close()
 
             else:
                 sys.stdout.write("No new version found.\n")
 
-outf = codecs.open(args.bibfile,'w','utf-8')
+outf = codecs.open(args.bibfile, 'w', 'utf-8')
 try:
     outf.write(newbib)
 except:
     outf.write(to_bibtex(bp))
 outf.close()
 
-sys.stdout.write(str(upcount)+' reference(s) updated.\n')
+sys.stdout.write(str(upcount) + ' reference(s) updated.\n')
--- Papers/macro_check.py
+++ Papers/macro_check.py
@@ -9,28 +9,32 @@ import os
 
 # extract list of macros to search for
 macros = {}
-if os.path.isfile(os.environ['HOME']+'/astro/software/Astronomy/Papers/privon_astro.sty'):
-    sty = open(os.environ['HOME']+'/astro/software/Astronomy/Papers/privon_astro.sty','r')
+if os.path.isfile(os.environ['HOME'] +
+                  '/astro/software/Astronomy/Papers/privon_astro.sty'):
+    sty = open(os.environ['HOME'] +
+               '/astro/software/Astronomy/Papers/privon_astro.sty', 'r')
     for line in sty:
-        if re.search('newcommand',line):
-            macros[line.split('{')[1].split('}')[0]]=line
+        if re.search('newcommand', line):
+            macros[line.split('{')[1].split('}')[0]] = line
     sty.close()
 
 texfile = sys.argv[1]
 
 if os.path.isfile(texfile):
-    inf = open(texfile,'r')
+    inf = open(texfile, 'r')
 else:
-    sys.stderr.write('Error opening: '+texfile+'. Not found.\n')
+    sys.stderr.write('Error opening: ' + texfile + '. Not found.\n')
 
 keeplist = {}
 for line in inf:
     for macro in macros.keys():
-        if re.search(macro,line):
+        if re.search(macro, line):
             keeplist[macro] = macros[macro]
             macros.pop(macro)
 inf.close()
 
-sys.stdout.write('Finished searching '+texfile+'. Please copy the following macros into your manuscript: \n\n')
+sys.stdout.write('Finished searching ' + texfile +
+                 '. Please copy the following macros into your manuscript: \
+                 \n\n')
 for macro in keeplist.keys():
     sys.stdout.write(keeplist[macro])
