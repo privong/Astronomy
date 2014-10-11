c7983e1327d98edf1355dc8a3283974c76e50037
George C. Privon <george@privon.com>
2014-10-10 23:36:18 -0300

    pep8

--- DataReduction/FixVLANames.py
+++ DataReduction/FixVLANames.py
@@ -5,17 +5,19 @@
 import os
 import string
 
+
 def FixVLANames(vis):
 
-  print "Changing antenna NAME column to modified name column for %s." % (vis)
-  newname=[]
-  # open the table of antenna names
-  tb.open(vis+'/ANTENNA',nomodify=False)
-  antenna=tb.getcol("NAME")
-  for ant in antenna:
-    ant="VA"+ant
-    newname.append(ant)
-  tb.putcol("NAME",newname)
-  tb.close
+    print "Changing antenna NAME column to modified name column for %s." % \
+          (vis)
+    newname = []
+    # open the table of antenna names
+    tb.open(vis + '/ANTENNA', nomodify=False)
+    antenna = tb.getcol("NAME")
+    for ant in antenna:
+        ant = "VA" + ant
+        newname.append(ant)
+    tb.putcol("NAME", newname)
+    tb.close
 
-  return 0
+    return 0
