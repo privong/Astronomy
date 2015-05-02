0f67209305cdc746311684207ae1b0c5ab5071b7
George C. Privon <george@privon.com>
2015-05-01 21:15:20 -0300

    finish implementation of retrieval of preprints to match published papers

--- Papers/bib_update.py
+++ Papers/bib_update.py
@@ -31,58 +31,83 @@ def checkRef(entry, confirm=False):
     """
     if 'eprint' in entry.keys():
         res = ads.query("arXiv:" + entry['eprint'])
-        for i in res:
-            try:    # only want things that are published
-                print i.pub
-                if re.search('arxiv', i.pub, re.IGNORECASE):
+        try:
+            for i in res:
+                try:    # only want things that are published
+                    print i.pub
+                    if re.search('arxiv', i.pub, re.IGNORECASE):
+                        continue
+                except:
                     continue
-            except:
-                continue
-            if confirm:
-                print entry['author'].split(',')[0], entry['title'], \
-                      entry['year']
-                print i.author[0], i.title[0], i.year
-                sel = raw_input('Is this a match (y/n)? ')
-            else:
-                sel = 'y'
-            if sel == 'y':  # replace relevant bibtex entries
-                entry['author'] = '{' + entry['author'] + '}'
-                entry['title'] = i.title[0]
-                entry['year'] = i.year
-                entry['journal'] = i.pub
-                try:
-                    entry['doi'] = i.doi[0]
-                except:
-                    pass
-                try:
-                    entry['abstract'] = i.abstract
-                except:
-                    pass
-                entry['link'] = i.url[0]
-                entry['year'] = i.year
-                try:
-                    entry['volume'] = i.volume
-                except:
-                    pass
-                try:
-                    entry['pages'] = i.page[0]
-                except:
-                    pass
-                entry['adsurl'] = 'http://adsabs.harvard.edu/abs/' + i.bibcode
-                if not(re.search(i.year, entry['id'])):
-                    sys.stderr.write("Warning: Updating year of: " +
-                                     entry['id'] +
-                                     " to reflect publication year (" +
-                                     i.year + ").\n")
-                    entry['id'] = entry['id'].split('2')[0] + i.year
-                return entry
+                if confirm:
+                    print entry['author'].split(',')[0], entry['title'], \
+                          entry['year']
+                    print i.author[0], i.title[0], i.year
+                    sel = raw_input('Is this a match (y/n)? ')
+                else:
+                    sel = 'y'
+                if sel == 'y':  # replace relevant bibtex entries
+                    entry['author'] = '{' + entry['author'] + '}'
+                    entry['title'] = i.title[0]
+                    entry['year'] = i.year
+                    entry['journal'] = i.pub
+                    try:
+                        entry['doi'] = i.doi[0]
+                    except:
+                        pass
+                    try:
+                        entry['abstract'] = i.abstract
+                    except:
+                        pass
+                    entry['link'] = i.url[0]
+                    entry['year'] = i.year
+                    try:
+                        entry['volume'] = i.volume
+                    except:
+                        pass
+                    try:
+                        entry['pages'] = i.page[0]
+                    except:
+                        pass
+                    entry['adsurl'] = 'http://adsabs.harvard.edu/abs/' + i.bibcode
+                    if not(re.search(i.year, entry['id'])):
+                        sys.stderr.write("Warning: Updating year of: " +
+                                         entry['id'] +
+                                         " to reflect publication year (" +
+                                         i.year + ").\n")
+                        entry['id'] = entry['id'].split('2')[0] + i.year
+                    return entry
+        except:
+            sys.stderr.write("API issue")
         return False
 
 def aref(entry, confirm=False):
     """
     Check ADS for a preprint associated with a published article.
     """
-
+    if 'doi' in entry.keys():
+        res = ads.query('doi:' + entry['doi'])
+        try:
+            for i in res:
+                try:
+                    for j in i.identifier:
+                        if re.search('arXiv:', j):
+                            ID = j.split(':')[1]
+                            entry['arxivid'] = ID
+                            entry['Eprint'] = ID
+                            entry['archiveprefix'] = 'arXiv'
+                            return entry
+                        elif re.search('astro-ph/', j):
+                            ID = j
+                            entry['arxivid'] = ID
+                            entry['Eprint'] = ID
+                            entry['archiveprefix'] = 'arXiv'
+                            return entry
+                except:
+                    pass
+        except:
+            return False
+    return False
 
 parser = argparse.ArgumentParser(description="Update arXiv entries in a bibtex \
                                  file with subsequently published papers.")
@@ -117,13 +142,13 @@ for j in range(len(bp.entries)):
         if 'journal' in thisref.keys():
             if re.search('arxiv', thisref['journal'], re.IGNORECASE):
                 match = True
-            elif args.arXiv:
-                aphserach = True
+            elif 'eprint' not in thisref.keys() and args.arXiv:
+                aphsearch = True
         elif 'Journal' in thisref.keys():
             if re.search('arxiv', thisref['Journal'], re.IGNORECASE):
                 match = True
-            elif args.arXiv:
-                aphserach = True
+            elif 'eprint' not in thisref.keys() and args.arXiv:
+                aphsearch = True
         else:
             if 'arxivid' in thisref.keys():
                 match = True
@@ -150,17 +175,16 @@ for j in range(len(bp.entries)):
             else:
                 sys.stdout.write("No new version found.\n")
 
-        if aphsearch:
+        if aphsearch and thisref['year'] >= '1991':
             aphsearch = False
             sys.stdout.write('No preprint associated with ' + thisref['id'] +
-                             ', checking for a preprint.')
+                             ', checking...\n')
             res = aref(thisref, args.confirm)
             if res:
                 acount += 1
                 bp.entries[j] = res
                 sys.stdout.write(thisref['id'] +
-                                 " updated with a preprint. Please verify \
-                                 changes.\n")
+                                 " updated with a preprint. Please verify changes.\n")
                 newbib = to_bibtex(bp)
                 if upcount + acount % 20 == 0:
                     outf = codecs.open(args.bibfile, 'w', 'utf-8')
@@ -177,4 +201,5 @@ except:
     outf.write(to_bibtex(bp))
 outf.close()
 
-sys.stdout.write(str(upcount) + ' reference(s) updated.\n')
+sys.stdout.write(str(upcount) + ' reference(s) updated with journal articles.\n')
+sys.stdout.write(str(acount) + ' reference(s) updated with preprints.\n')
