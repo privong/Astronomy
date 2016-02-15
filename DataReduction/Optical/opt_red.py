f5e701d7a258e24a206b44a208b376466000052d
George C. Privon <accounts5@privon.com>
2016-02-15 15:23:18 -0300

    put image combining into a function, to simplify code

--- DataReduction/Optical/opt_red.py
+++ DataReduction/Optical/opt_red.py
@@ -21,6 +21,92 @@ import mysci
 import argparse
 
 
+def imageCombine(imgnames, frametype, verbose=False):
+    """
+    Take a list of images, perform a specified combination operation and
+    return the final image. Optionally provide diagnostic feedback.
+    """
+    
+    mean = []
+    median = []
+    stddev = []
+    cframes = np.array([0])
+    for image in imgnames:
+        idata = mysci.Telload(image, Tel=args.telescope,
+                              quiet=not(args.verbose))
+        if cframes.ndim < 2:
+            cframes = idata.copy()
+        else:
+            cframes = np.dstack((cframes, idata))
+        stddev.append(np.std(idata))
+
+        mean.append(np.mean(idata))
+        median.append(np.median(idata))
+        if args.verbose:
+            sys.stderr.write('\nMean: ' + str(np.mean(idata)) +
+                             '\tMedian: ' + str(np.median(idata)) +
+                             '\tstddev: '+str(np.std(idata)))
+            sys.stderr.write('.\n')
+
+    sys.stderr.write('\n')
+    sys.stderr.write('Loaded ' + str(len(mean)) + ' ' + frametype + ' frames.\n')
+    if verbose:
+        # plot diagonstics
+        fig = plt.figure()
+        # plt.scatter(range(len(mean)),mean,color='red',label='Mean')
+        plt.scatter(range(len(median)), median, color='blue', label='Median')
+        plt.scatter(range(len(stddev)), stddev, color='green', label='Stddev')
+        plt.legend()
+        plt.show()
+    # right now take all the frames, need to add selective frame-ignoring
+    mframe = np.mean(cframes, axis=2)
+    if args.verbose:
+        sys.stderr.write(str(cframes.shape) + '\n')
+        sys.stderr.write(str(mframe.shape) + '\n')
+
+    datenow = datetime.datetime.today().isoformat()
+
+    # if we're dealing with a single fits extension, create a new file
+    fname = 'master' + frametype + '.fits'
+    if os.path.isfile(fname):
+        sys.stderr.write('Deleting existing ' + fname + '\n')
+        raw_input('PRESS ENTER TO CONTINUE')
+        os.remove(fname)
+    if len(mframe.shape) < 3:
+        # copy header from an existing frame, add comments saying which
+        # frames were combined and how they were combined.
+        frame = pyfits.PrimaryHDU(np.transpose(mframe))
+        sframe = pyfits.open(image)
+        # copy over the keywords we want
+        for kw in kprop:
+            if kw in sframe[0].header:
+                frame.header.set(kw, sframe[0].header[kw])
+        # add reduction info to header
+        frame.header['IMAGETYP'] = fname.split('.fits')[0]
+        frame.header.add_history(datenow + ' - master ' + frametype + ' created')
+        frame.header.add_comment('Master ' + frametype + ' created from mean of: ' +
+                                 str(imgnames))
+        frame.writeto(fname)
+    else:
+        # we need to insert this new data into a copy of the old fits file
+        shutil.copy(image, fname)
+        frame = pyfits.open(fname, mode='update')
+        frame[0].header['IMAGETYP'] = fname.split('.fits')[0]
+        frame[0].header.add_history(datenow + ' - master ' + frametype + ' created')
+        frame[0].header.add_comment('Master ' + frametype + ' created from mean of: ' +
+                                    str(imgnames))
+        for hdu in range(1, len(frame)):
+            if verbose:
+                sys.stderr.write("Writing HDU " + str(hdu) + " with mean: " +
+                                 str(np.mean(mframe[hdu-1])) + '\n')
+            frame[hdu].header['BZERO'] = 0.0    # don't rescale data
+            frame[hdu].data = np.transpose(mframe[hdu-1, :, :])
+        frame.close()
+
+    sys.stderr.write('\n')
+    del cframes 
+
+
 parser = argparse.ArgumentParser(description='Pipeline to generate master \
 bias and flat frames, then apply those master calibration files to a batch \
 of science observations.')
@@ -106,95 +192,10 @@ Exiting.\n\n')
                          args.masterbias+'\n\n')
         mbias = mysci.Telload(args.masterbias, Tel=args.telescope,
                               quiet=not(args.verbose))
-else:   # make a master bias
-    mean = []
-    median = []
-    stddev = []
-    bframes = np.array([0])
-    sys.stderr.write('Creating a master bias file...\n')
-    sys.stderr.write('Loading bias frames...\n')
-    for image in bias:
-        # need to un-fix this later when more telescopes are added
-        idata = mysci.Telload(image, Tel=args.telescope,
-                              quiet=not(args.verbose))
-        # idata=idata.reshape((1,idata.shape[0],idata.shape[1],idata.shape[2]))
-        if bframes.ndim < 2:
-            # bframes.resize((1,idata.shape[0],idata.shape[1],idata.shape[2]))
-            bframes = idata.copy()
-        else:
-            bframes = np.concatenate((bframes, idata), axis=0)
-        stddev.append(np.std(idata))
-
-        """
-        this is wrong for large files unless the data are converted to float64
-        since this isn't necessary for the other steps, we won't do it
-        """
-        mean.append(np.mean(idata))
-        median.append(np.median(idata))
-        if args.verbose:
-            sys.stderr.write('\nMean: ' + str(np.mean(idata)) +
-                             '\tMedian: ' + str(np.median(idata)) +
-                             '\tstddev: '+str(np.std(idata)))
-            sys.stderr.write('.\n')
-
-    sys.stderr.write('\n')
-    sys.stderr.write('Loaded ' + str(len(mean)) + ' bias frames.\n')
-    if args.plot:
-        # plot diagonstics
-        fig = plt.figure()
-        # plt.scatter(range(len(mean)),mean,color='red',label='Mean')
-        plt.scatter(range(len(median)), median, color='blue', label='Median')
-        plt.scatter(range(len(stddev)), stddev, color='green', label='Stddev')
-        plt.legend()
-        plt.show()
-    # right now take all the frames, need to add selective frame-ignoring
-    mbias = np.mean(bframes, axis=0)
-    if args.verbose:
-        sys.stderr.write(str(bframes.shape) + '\n')
-        sys.stderr.write(str(mbias.shape) + '\n')
-
-    datenow = datetime.datetime.today().isoformat()
-
-    # if we're dealing with a single fits extension, create a new file
-    if os.path.isfile('masterbias.fits'):
-        sys.stderr.write('Deleting existing masterbias.fits\n')
-        raw_input('PRESS ENTER TO CONTINUE')
-        os.remove('masterbias.fits')
-    if len(mbias.shape) < 3:
-        # copy header from an existing bias frame, add comments saying which
-        # bias frames were combined and how they were combined.
-        frame = pyfits.PrimaryHDU(mbias)
-        sbias = pyfits.open(image)
-        # copy over the keywords we want
-        for kw in kprop:
-            if kw in sbias[0].header:
-                frame.header.set(kw, sbias[0].header[kw])
-        # add reduction info to header
-        frame.header['IMAGETYP'] = 'masterbias'
-        frame.header.add_history(datenow + ' - Masterbias created')
-        frame.header.add_comment('Master bias created from mean of: ' +
-                                 str(bias))
-        frame.writeto('masterbias.fits')
-    else:
-        # we need to insert this new data into a copy of the old fits file
-        shutil.copy(image, 'masterbias.fits')
-        frame = pyfits.open('masterbias.fits', mode='update')
-        frame[0].header['IMAGETYP'] = 'masterbias'
-        frame[0].header.add_history(datenow + ' - Masterbias created')
-        frame[0].header.add_comment('Master bias created from mean of: ' +
-                                    str(bias))
-        for hdu in range(1, len(frame)):
-            if args.verbose:
-                sys.stderr.write("Writing HDU " + str(hdu) + " with mean: " +
-                                 str(np.mean(mbias[hdu-1])) + '\n')
-                # sys.stderr.write(str(mbias[hdu-1,:,:].shape)+'\n')
-                # sys.stderr.write(str(mbias[hdu-1,:,:].dtype)+'\n')
-            frame[hdu].header['BZERO'] = 0.0    # don't rescale data
-            frame[hdu].data = np.transpose(mbias[hdu-1, :, :])
-        frame.close()
-
-    sys.stderr.write('\n')
-    del bframes
+else:   # make a master bias and load it
+    imageCombine(bias, 'bias', verbose=args.verbose)
+    mbias = mysci.Telload('masterbias.fits', Tel=args.telescope,
+                          quiet=not(args.verbose))
 
 sys.stderr.write('\n\n\nNOTE: We are ignoring dark frames at the \
 moment!!!!!\n\n\n')
@@ -240,83 +241,15 @@ mflats = np.array([0])
 # Create a master flat
 # first load all the flat files (and normalize them), then loop over filters
 sys.stderr.write('Creating master flats.\n')
-for filter in filters:
-    sys.stderr.write("Making master flat field for filter: " + filter + ' ')
+for filtername in filters:
+    sys.stderr.write("Making master flat field for filter: " + filtername + ' ')
     fframes = np.array([0])
     stddev = []
+    thisfiltfiles = []
     for image in flats:
-        if re.match(filter, image[1]):
-            idata = mysci.Telload(image[0], Tel=args.telescope,
-                                  quiet=not(args.verbose))
-            # idata.reshape((1,idata.shape[0],idata.shape[1],idata.shape[2]))
-            if args.verbose:
-                sys.stderr.write('\nmbias.shape=' + str(mbias.shape) +
-                                 '\tidata.shape=' + str(idata.shape)+'\n\n')
-            idata = idata-mbias
-            # idata.resize(1,idata.shape[0],idata.shape[1],idata.shape[2])
-            # add that flat field image onto the stack
-            if fframes.ndim < 2:
-                fframes = idata/np.median(idata)
-            else:
-                fframes = np.concatenate((fframes, idata/np.median(idata)),
-                                         axis=0)
-            stddev.append(np.std(idata/np.mean(idata)))
-    sys.stderr.write('\n')
-    # we've loaded all the flats for that particular filter, now make a master
-    mflat = np.median(fframes, axis=0)
-    if args.verbose:
-        sys.stderr.write('fframes.shape: ' + str(fframes.shape) +
-                         '\tmflat.shape: ' + str(mflat.shape)+'\n')
-    mfname = filter + '_masterflat.fits'
-    # add that master flat to the stack of master flats
-    if mflats.ndim < 2:
-        mflats = mflat*1.0
-        # mflats.resize((1,mflat.shape[0],mflat.shape[1],mflat.shape[2]))
-    else:
-        mflats = np.concatenate((mflats, mflat), axis=0)
-    if os.path.isfile(mfname):
-        sys.stderr.write('Deleting existing ' + mfname+'\n')
-        raw_input('PRESS ENTER TO CONTINUE')
-        os.remove(mfname)
-    datenow = datetime.datetime.today().isoformat()
-    if args.verbose:
-        sys.stderr.write("mflats.shape: " + str(mflats.shape) + "\n")
-    # if we're dealing with a single fits extension, create a new file
-    if mflat.ndim < 2:
-        if args.verbose:
-            sys.stderr.write('Single extension data, \
-creating a new fits file.\n')
-        frame = pyfits.PrimaryHDU(mflat)
-        sflat = pyfits.open(image[0])
-        for kw in kprop:
-            if kw in sflat[0].header:
-                frame.header.set(kw, sflat[0].header[kw])
-        frame.header['IMAGETYP'] = 'masterflat'
-        frame.header.add_history(datenow + ' - Master Flat created')
-        sflat.close()
-        sys.stderr.write('\tsaving master flat as: ')
-        sys.stderr.write(mfname + '\n')
-        frame.writeto(mfname)
-    # otherwise we insert this new data into a copy of the old fits file
-    else:
-        if args.verbose:
-            sys.stderr.write('Multiextension data, copying off an \
-existing image\n')
-        shutil.copy(image[0], mfname)
-        frame = pyfits.open(mfname, mode='update')
-        frame[0].header['IMAGETYP'] = 'masterflat'
-        frame[0].header.add_history(datenow + ' - Master Flat created')
-        for hdu in range(1, len(frame)):
-            if args.verbose:
-                sys.stderr.write("Writing HDU " + str(hdu) + " with mean: " +
-                                 str(np.mean(mflat[hdu-1])) + '\t')
-                sys.stderr.write(str(mflat[hdu-1, :, :].shape) + '\t')
-                sys.stderr.write(str(mflat[hdu-1, :, :].dtype) + '\n')
-            frame[hdu].header['BZERO'] = 0.0    # so the data isn't rescaled
-            frame[hdu].data = np.transpose(mflat[hdu-1, :, :])
-        frame.close()
-
-del fframes
+        if re.match(filtername, image[1]):
+            thisfiltfiles.append(image[0])
+    imageCombine(thisfiltfiles, "flat_"+filtername, verbose=args.verbose)
 
 # now process the individual object frames, going by filter
 sys.stderr.write('\n\nProcessing object frames (bias and flat correction)\n')
